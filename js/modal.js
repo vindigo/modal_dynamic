99f47082270415e597330232325e697aa8621e19
Rich Frausto <vindigo@gmail.com>
2018-02-23 14:55:28 -0800

    added basic form

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,40 @@
 
 // elements
 var modal = document.getElementById('modal');
-var modalBtn = document.getElementById('modalBtn');
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
+var modalMiddle = document.getElementsByClassName('middle')[0];
+
+var modalTopBtn = document.getElementById('modalTopBtn');
+var modalMiddleBtn = document.getElementById('modalMiddleBtn');
+var modalBottomBtn = document.getElementById('modalBottomBtn');
+
+var submitBtn = document.getElementById('submitBtn');
+var closeBtn = document.getElementById('cancelBtn');
 
 // events
-modalBtn.addEventListener('click', openModal);
+modalTopBtn.addEventListener( 'click', openModalTop );
+modalMiddleBtn.addEventListener( 'click', openModalMiddle );
+modalBottomBtn.addEventListener( 'click', openModalBottom );
+
+submitBtn.addEventListener('click', closeModal);
 closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', clickOutsideModal)
+// window.addEventListener('click', clickOutsideModal) optional
 
 // functions
+function openModalTop(){
+  modalMiddle.style.verticalAlign = "top";
+  openModal();
+}
+
+function openModalMiddle(){
+  modalMiddle.style.verticalAlign = "middle";
+  openModal();
+}
+
+function openModalBottom(){
+  modalMiddle.style.verticalAlign = "bottom";
+  openModal();
+}
+
 function openModal(){
   modal.style.display = 'block';
 }
