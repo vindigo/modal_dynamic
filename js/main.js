36e4e38b41fb3d7c69355ffa84db296db58ad1c2
Rich Frausto <vindigo@gmail.com>
2018-03-12 11:08:44 -0700

    added ajax call

--- js/main.js
+++ js/main.js
@@ -1,51 +1,53 @@
-var content =
-  '<div class="formContainer">' +
-  '<form>' +
-  '<div class="row">' +
-  '<div class="col-25">' +
-  '<label for="fname">First Name</label>' +
-  '</div>' +
-  '<div class="col-75">' +
-  '<input type="text" id="fname" name="firstname" placeholder="Your name..">' +
-  '</div>' +
-  '</div>' +
-  '<div class="row">' +
-  '<div class="col-25">' +
-  '<label for="lname">Last Name</label>' +
-  '</div>' +
-  '<div class="col-75">' +
-  '<input type="text" id="lname" name="lastname" placeholder="Your last name..">' +
-  '</div>' +
-  '</div>' +
-  '<div class="row">' +
-  '<div class="col-25">' +
-  '<label for="subject">Subject</label>' +
-  '</div>' +
-  '<div class="col-75">' +
-  '<textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>' +
-  '</div>' +
-  '</div>' +
-  '</form>' +
-  '</div>'
+// var content =
+//   '<div class="formContainer">' +
+//   '<form>' +
+//   '<div class="row">' +
+//   '<div class="col-25">' +
+//   '<label for="fname">First Name</label>' +
+//   '</div>' +
+//   '<div class="col-75">' +
+//   '<input type="text" id="fname" name="firstname" placeholder="Your name..">' +
+//   '</div>' +
+//   '</div>' +
+//   '<div class="row">' +
+//   '<div class="col-25">' +
+//   '<label for="lname">Last Name</label>' +
+//   '</div>' +
+//   '<div class="col-75">' +
+//   '<input type="text" id="lname" name="lastname" placeholder="Your last name..">' +
+//   '</div>' +
+//   '</div>' +
+//   '<div class="row">' +
+//   '<div class="col-25">' +
+//   '<label for="subject">Subject</label>' +
+//   '</div>' +
+//   '<div class="col-75">' +
+//   '<textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>' +
+//   '</div>' +
+//   '</div>' +
+//   '</form>' +
+//   '</div>'
 
-var msg = "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+// var msg = "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
+
+var modalBtn = document.getElementById('modalBtn');
 
 document.onreadystatechange = function() {
   if (document.readyState == "interactive") {
-    var config = {
-      position: 'middle',
-      content: content,
-      submitBtn: true
-    }
 
-    var modalBtn = document.getElementById('modalBtn');
+    modalBtn.addEventListener("click", function(){
+        loadData("data/lorem.html", function(xhttp){
+          var config = {
+              position: 'middle',
+              content: xhttp.responseText,
+              submitBtn: true
+            }
 
-    if (modalBtn) {
-      modalBtn.addEventListener('click', function() {
-        var myModal = new objModal(config);
+          var myModal = new objModal(config)
+          myModal.openModal();
 
-        myModal.openModal();
-      });
-    }
+        })
+    });
+
   }
 }
